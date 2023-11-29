6343dc38afbf5d94ed89a07663e5da1bdd9ec52d
VARMAMASTER <118650885+VARMAMASTER@users.noreply.github.com>
2023-11-29 16:11:49 +0530

    edit area is removed and the home and paage code modified then added functionality to add text to the main canvas page

--- lib/components/page.dart
+++ lib/components/page.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
 
 class page extends StatefulWidget {
-  const page({super.key});
+  const page({Key? key, this.addedText = ''}) : super(key: key);
+
+  final String addedText;
 
   @override
-  State<page> createState() => _pageState();
+  _pageState createState() => _pageState();
 }
 
 class _pageState extends State<page> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(15),
       child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            color: const Color.fromARGB(255, 255, 238, 187)),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          color: Color.fromARGB(255, 255, 238, 187),
+        ),
         width: 400,
         height: 480,
-        // ,
+        child: Center(
+          child: Text(
+            widget.addedText,
+            style: const TextStyle(fontSize: 20, color: Colors.black),
+          ),
+        ),
       ),
     );
   }
--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,18 @@
-import 'package:celebrare/components/editArea.dart';
+import 'package:flutter/material.dart';
 import 'package:celebrare/components/editingTools.dart';
 import 'package:celebrare/components/page.dart';
 
-import 'package:flutter/material.dart';
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String addedText = "";
+  TextEditingController textController = TextEditingController();
+  bool Text_active = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,13 +21,9 @@ class Home extends StatelessWidget {
         title: const Text("Designer-Celebrare"),
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.undo)),
-          const SizedBox(
-            width: 5,
-          ),
+          const SizedBox(width: 5),
           IconButton(onPressed: () {}, icon: const Icon(Icons.redo)),
-          const SizedBox(
-            width: 20,
-          ),
+          const SizedBox(width: 20),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
@@ -37,25 +39,70 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          page(),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 0, 71, 225),
-            ),
-            child: const Text(
-              "add Text",
-              style: TextStyle(color: Colors.white, fontSize: 22),
-            ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color.fromARGB(255, 82, 166, 236),
+          height: MediaQuery.of(context).size.height -
+              AppBar().preferredSize.height -
+              12,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              page(addedText: addedText),
+              Visibility(
+                visible: true,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _openTextEditor();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 0, 71, 225),
+                  ),
+                  child: const Text(
+                    "Add Text",
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  ),
+                ),
+              ),
+              Tools(),
+            ],
           ),
-          editArea(),
-          Tools(),
-        ],
+        ),
       ),
+      resizeToAvoidBottomInset: false,
+    );
+  }
+
+  void _openTextEditor() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Add Text"),
+          content: TextField(
+            controller: textController,
+            decoration: const InputDecoration(labelText: 'Enter Text'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  addedText = textController.text;
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
